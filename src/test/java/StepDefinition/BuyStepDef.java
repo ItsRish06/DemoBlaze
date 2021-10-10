779e2df28c2db70c92d9859d5e2aa86dcebc3da5
ItsRish06 <66073563+ItsRish06@users.noreply.github.com>
2021-10-10 17:26:21 +0530

    Buy feature , buyPage , buyStepDef

--- src/test/java/StepDefinition/BuyStepDef.java
+++ src/test/java/StepDefinition/BuyStepDef.java
@@ -1,33 +1,38 @@
 package StepDefinition;
 
 import com.base.Base;
+import com.page.BuyPage;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 public class BuyStepDef extends Base {
 
+    BuyPage bp;
+
+    @Before("@buy")
+    public void initial(){
+        initialization();
+        bp = new BuyPage();
+    }
 
     @Given("^b\\-User is into cart$")
     public void buser_is_into_cart() throws Throwable {
-        initialization();
         getPage("https://www.demoblaze.com/cart.html");
     }
 
     @When("^b\\-User clicks PlaceOrder$")
     public void buser_clicks_placeorder() throws Throwable {
-        driver.findElement(By.xpath("//div[@id='page-wrapper']/div/div[2]/button")).click();
+        bp.placeOrder();
     }
 
     @Then("^b\\-User Gets Popup For Details$")
     public void buser_gets_popup_for_details() throws Throwable {
-        WebDriverWait wait = new WebDriverWait(driver,10);
-        String actualResult = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='orderModal']/div/div/div"))).getText();
+        String actualResult = bp.validateDetailPopup();
         System.out.println(actualResult);
         String expectedResult = "Place order";
         Assert.assertEquals(actualResult,expectedResult);
@@ -36,37 +41,27 @@ public class BuyStepDef extends Base {
 
     @Given("^b\\-User is on cart and Clicks PlaceOrder$")
     public void buser_is_on_cart_and_clicks_placeorder() throws Throwable {
-        initialization();
         getPage("https://www.demoblaze.com/cart.html");
-        driver.findElement(By.xpath("//div[@id='page-wrapper']/div/div[2]/button")).click();
+        bp.placeOrder();
     }
 
 
 
     @When("^b-User Enters (.+) (.+) (.+) (.+) (.+) (.+)$")
     public void buser_enters(String name, String country, String city, String creditcard, String month, String year) throws Throwable {
-        WebDriverWait wait = new WebDriverWait(driver,10);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='orderModal']/div/div")));
-        driver.findElement(By.id("name")).sendKeys(name);
-        driver.findElement(By.id("country")).sendKeys(country);
-        driver.findElement(By.id("city")).sendKeys(city);
-        driver.findElement(By.id("card")).sendKeys(creditcard);
-        driver.findElement(By.id("month")).sendKeys(month);
-        driver.findElement(By.id("year")).sendKeys(year);
+        bp.enterDetails(name,country,city,creditcard,month,year);
 
     }
 
     @And("^b\\-User Clicks Purchase$")
     public void buser_clicks_purchase() throws Throwable {
-        driver.findElement(By.xpath("//div[@id='orderModal']/div/div/div[3]/button[2]")).click();
+        bp.clickPurchase();
     }
 
 
-    @Then("^b\\-Validate successfull purchase$")
+    @Then("^b\\-Validate successful purchase$")
     public void bvalidate_successfull_purchase() throws Throwable {
-        WebDriverWait wait = new WebDriverWait(driver,10);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='sweet-alert  showSweetAlert visible']")));
-        String actualResult = driver.findElement(By.xpath("//div[@class='sweet-alert  showSweetAlert visible']/h2")).getText();
+        String actualResult = bp.validatePurchase();
         String expectedResult = "Thank you for your purchase!";
         System.out.println(actualResult);
         Assert.assertEquals(actualResult,expectedResult);
@@ -76,13 +71,17 @@ public class BuyStepDef extends Base {
     public void bvalidate_unsuccessful_purchase() throws Throwable {
         boolean result;
         try{
-            WebDriverWait wait = new WebDriverWait(driver,10);
-            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='sweet-alert  showSweetAlert visible']")));
+            String actual = bp.validatePurchase();
             result=true;
         }catch (Exception e){
           result = false;
         }
         Assert.assertFalse(result);
+    }
+
+    @After("@buy")
+    public void finish(){
+        tearDown();
     }
 
 }
